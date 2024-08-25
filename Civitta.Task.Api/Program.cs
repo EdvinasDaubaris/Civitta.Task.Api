6c98a001b26ce43510a7b238565c2cfdf7001eba
EdvinasDaubaris <edvinas@viacorex.com>
2024-08-25 14:12:41 +0300

    .

--- Civitta.Task.Api/Program.cs
+++ Civitta.Task.Api/Program.cs
@@ -27,9 +27,6 @@ builder.Services.AddDbContext<DatabaseContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
 
 //app.UseAuthorization();
 app.UseFastEndpoints().UseSwaggerGen();
